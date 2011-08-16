e0ff24dfa212a14659660b789c4a66505deda34e
Tobias Dittrich <dittrich@alternate.de>
2011-08-16 17:03:38 +0200

    wip

--- src/de/tle/dso/sim/Simulation.java
+++ src/de/tle/dso/sim/Simulation.java
@@ -7,39 +7,63 @@ import de.tle.dso.sim.battle.BattleResult;
 import de.tle.dso.sim.battle.InvalidArmyException;
 import de.tle.dso.units.Army;
 import de.tle.dso.units.util.UnitPatternHelper;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 public class Simulation {
-  private static int MAX_RUNS = 10000;
 
+  private static int MAX_RUNS = 10000;
   private String attackingArmyPattern;
   private String defendingArmyPattern;
   private int numberOfRounds;
   private SimulationResult simResult;
-  private Army attackingArmy;
-  private Army defendingArmy;
   private final static Logger LOG = Logger.getLogger(false);
+  private final ThreadPoolExecutor threadPool;
 
   public Simulation(String attackingArmyPattern, String defendingArmyPattern, int numberOfRounds) {
     this.attackingArmyPattern = attackingArmyPattern;
     this.defendingArmyPattern = defendingArmyPattern;
     this.numberOfRounds = numberOfRounds;
     this.simResult = new SimulationResult();
+
+    int numberOfThreads = Runtime.getRuntime().availableProcessors() * 2;
+    LOG.info("Number of processors: %s", numberOfThreads);
+
+    this.threadPool = new ThreadPoolExecutor(numberOfThreads, numberOfThreads, 1, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>());
   }
 
   public SimulationResult simulate() throws InvalidArmyException {
+    List<BattleSimulatorRunner> listOfBattleRunners = new LinkedList<BattleSimulatorRunner>();
+
     for (int i = 0; i < numberOfRounds; i++) {
-      createArmies();
-      Battle battle = new Battle(attackingArmy, defendingArmy);
-      BattleResult roundResult = battle.simulateBattle();
-      simResult.addBattleResult(roundResult);
+      Army attackingArmy = UnitPatternHelper.createArmyFromPattern(attackingArmyPattern);
+      Army defendingArmy = UnitPatternHelper.createArmyFromPattern(defendingArmyPattern);
+
+      listOfBattleRunners.add(new BattleSimulatorRunner(attackingArmy, defendingArmy));
+    }
+
+    try {
+      List<Future<BattleResult>> results = threadPool.invokeAll(listOfBattleRunners);
+      for (Future<BattleResult> battleResult : results) {
+        try {
+          simResult.addBattleResult(battleResult.get());
+        } catch (ExecutionException ex) {
+          java.util.logging.Logger.getLogger(Simulation.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      }
+    } catch (InterruptedException ex) {
+      // ignore
     }
 
     return simResult;
-  }
-
-  private void createArmies() {
-    attackingArmy = UnitPatternHelper.createArmyFromPattern(attackingArmyPattern);
-    defendingArmy = UnitPatternHelper.createArmyFromPattern(defendingArmyPattern);
   }
 
   public static void main(String[] args) throws InvalidArmyException {
@@ -54,6 +78,23 @@ public class Simulation {
     LOG.info("Min Computer losses: %s", UnitPatternHelper.createPatternFromMap(simResult.getMinComputerLosses()));
     LOG.info("Max Computer losses: %s", UnitPatternHelper.createPatternFromMap(simResult.getMaxComputerLosses()));
     LOG.info("Time: %s s", (System.currentTimeMillis() - start) / 1000);
-    LogManager.shutdown();
+    System.exit(0);
+  }
+
+  private static class BattleSimulatorRunner implements Callable<BattleResult> {
+
+    private Army attackingArmy;
+    private Army defendingArmy;
+
+    public BattleSimulatorRunner(Army attackingArmy, Army defendingArmy) {
+      this.attackingArmy = attackingArmy;
+      this.defendingArmy = defendingArmy;
+    }
+
+    @Override
+    public BattleResult call() throws Exception {
+      Battle battle = new Battle(attackingArmy, defendingArmy);
+      return battle.simulateBattle();
+    }
   }
 }
