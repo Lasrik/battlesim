06f5ac168ab5586efcf619a83e09ed333f6de76d
Tobias Dittrich <dittrich@alternate.de>
2011-08-17 10:00:13 +0200

    wip

--- src/de/tle/dso/sim/Simulation.java
+++ src/de/tle/dso/sim/Simulation.java
@@ -3,72 +3,46 @@ package de.tle.dso.sim;
 import com.spinn3r.log5j.LogManager;
 import com.spinn3r.log5j.Logger;
 import de.tle.dso.sim.battle.Battle;
-import de.tle.dso.sim.battle.BattleResult;
 import de.tle.dso.sim.battle.InvalidArmyException;
 import de.tle.dso.units.Army;
 import de.tle.dso.units.util.UnitPatternHelper;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
 
 public class Simulation {
 
-  private static int MAX_RUNS = 10000;
+  public final static int MAX_RUNS = 50;
   private String attackingArmyPattern;
   private String defendingArmyPattern;
   private int numberOfRounds;
   private SimulationResult simResult;
   private final static Logger LOG = Logger.getLogger(false);
-  private final ThreadPoolExecutor threadPool;
 
-  public Simulation(String attackingArmyPattern, String defendingArmyPattern, int numberOfRounds) {
+  public Simulation(String attackingArmyPattern, String defendingArmyPattern) {
     this.attackingArmyPattern = attackingArmyPattern;
     this.defendingArmyPattern = defendingArmyPattern;
-    this.numberOfRounds = numberOfRounds;
+    this.numberOfRounds = MAX_RUNS;
     this.simResult = new SimulationResult();
-
-    int numberOfThreads = Runtime.getRuntime().availableProcessors();
-
-    this.threadPool = new ThreadPoolExecutor(numberOfThreads, numberOfThreads, 1, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>());
   }
 
   public SimulationResult simulate() throws InvalidArmyException {
-    for (int rounds = 0; rounds < (numberOfRounds / 100); rounds++) {
-      List<BattleRunner> listOfBattleRunners = new LinkedList<BattleRunner>();
-
-      for (int i = 0; i < 100; i++) {
-        Army attackingArmy = UnitPatternHelper.createArmyFromPattern(attackingArmyPattern);
-        Army defendingArmy = UnitPatternHelper.createArmyFromPattern(defendingArmyPattern);
-
-        listOfBattleRunners.add(new BattleRunner(attackingArmy, defendingArmy));
-      }
-
-      try {
-        List<Future<BattleResult>> results = threadPool.invokeAll(listOfBattleRunners);
-        for (Future<BattleResult> battleResult : results) {
-          simResult.addBattleResult(battleResult.get());
-        }
-      } catch (Exception ex) {
-        LOG.error("Exception in simulation: ", ex);
-      }
+    for (int rounds = 0; rounds < numberOfRounds; rounds++) {
+      Army attackingArmy = createArmy(attackingArmyPattern);
+      Army defendingArmy = createArmy(defendingArmyPattern);
+      Battle battle = new Battle(attackingArmy, defendingArmy);
+      simResult.addBattleResult(battle.simulateBattle());
     }
 
-    threadPool.shutdown();
-
     return simResult;
+  }
+
+  private Army createArmy(String pattern) {
+    Army attackingArmy = UnitPatternHelper.createArmyFromPattern(pattern);
+    return attackingArmy;
   }
 
   public static void main(String[] args) throws InvalidArmyException {
     LOG.info("Starting Simulation ....");
     long start = System.currentTimeMillis();
-    Simulation sim = new Simulation("200 S, 1G", "160 RB, 10 WL, 30 WH", MAX_RUNS);
+    Simulation sim = new Simulation("200 S, 1G", "160 RB, 10 WL, 30 WH");
     SimulationResult simResult = sim.simulate();
 
     LOG.info("Done %s Runs", simResult.getNumberOfSimulationRuns());
@@ -78,26 +52,5 @@ public class Simulation {
     LOG.info("Max Computer losses: %s", UnitPatternHelper.createPatternFromMap(simResult.getMaxComputerLosses()));
     LOG.info("Time: %s s", (System.currentTimeMillis() - start) / 1000);
     LogManager.shutdown();
-
-
-
-
-  }
-
-  private static class BattleRunner implements Callable<BattleResult> {
-
-    private Army attackingArmy;
-    private Army defendingArmy;
-
-    public BattleRunner(Army attackingArmy, Army defendingArmy) {
-      this.attackingArmy = attackingArmy;
-      this.defendingArmy = defendingArmy;
-    }
-
-    @Override
-    public BattleResult call() throws Exception {
-      Battle battle = new Battle(attackingArmy, defendingArmy);
-      return battle.simulateBattle();
-    }
   }
 }
--- test/de/tle/dso/sim/SimulationTest.java
+++ test/de/tle/dso/sim/SimulationTest.java
@@ -9,20 +9,18 @@ import static org.junit.Assert.*;
 
 public class SimulationTest {
 
-  public static final int MAX_RUNS = 100;
-
   @Before
   public void setUp() {
   }
 
   @Test
   public void testSimulate() throws Exception {
-    Simulation sim = new Simulation("200 E, 1G", "160 RB, 10 WL, 30 WH", MAX_RUNS);
+    Simulation sim = new Simulation("200 E, 1G", "160 RB, 10 WL, 30 WH");
     SimulationResult simResult = sim.simulate();
 
     assertNotNull(simResult);
 
-    assertEquals(MAX_RUNS, simResult.getNumberOfSimulationRuns());
+    assertEquals(Simulation.MAX_RUNS, simResult.getNumberOfSimulationRuns());
 
     assertTrue(91 <= simResult.getMinPlayerLosses().get(Elitesoldat.class));
     assertTrue(104 >= simResult.getMaxPlayerLosses().get(Elitesoldat.class));
@@ -30,12 +28,12 @@ public class SimulationTest {
 
   @Test
   public void testSimulate2() throws Exception {
-    Simulation sim = new Simulation("200 S, 1G", "160 RB, 10 WL, 30 WH", MAX_RUNS);
+    Simulation sim = new Simulation("200 S, 1G", "160 RB, 10 WL, 30 WH");
     SimulationResult simResult = sim.simulate();
 
     assertNotNull(simResult);
 
-    assertEquals(MAX_RUNS, simResult.getNumberOfSimulationRuns());
+    assertEquals(Simulation.MAX_RUNS, simResult.getNumberOfSimulationRuns());
 
     assertTrue(115 <= simResult.getMinPlayerLosses().get(Soldat.class));
     assertTrue(128 >= simResult.getMaxPlayerLosses().get(Soldat.class));
@@ -43,12 +41,12 @@ public class SimulationTest {
 
   @Test
   public void testSimulate3() throws Exception {
-    Simulation sim = new Simulation("70 S, 130 LB, 1G", "1 DWW", MAX_RUNS);
+    Simulation sim = new Simulation("70 S, 130 LB, 1G", "1 DWW");
     SimulationResult simResult = sim.simulate();
 
     assertNotNull(simResult);
 
-    assertEquals(MAX_RUNS, simResult.getNumberOfSimulationRuns());
+    assertEquals(Simulation.MAX_RUNS, simResult.getNumberOfSimulationRuns());
 
     assertEquals(1, (int) simResult.getMinComputerLosses().get(Waltraud.class));
     assertEquals(1, (int) simResult.getMaxComputerLosses().get(Waltraud.class));
