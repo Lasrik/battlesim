d2ab7e3d0422f8c1666841473dcf67e5a91807e5
Tobias Dittrich <dittrich@alternate.de>
2011-08-18 11:43:16 +0200

    wip

--- src/de/tle/dso/resources/Resource.java
+++ src/de/tle/dso/resources/Resource.java
@@ -1,5 +1,25 @@
 package de.tle.dso.resources;
 
-public class Resource {
-  
+public enum Resource {
+  SETTLER(1),
+  BEER(1),
+  BRONCE_SWORD(1),
+  BOW(2),
+  HORSE(4),
+  IRON_SWORD(5),
+  STEEL_SWORD(8),
+  LONG_BOW(4),
+  TITAN_SWORD(15),
+  CROSSBOW(10),
+  CANON(20);
+  final int weight;
+
+  Resource(int weight) {
+    this.weight = weight;
+  }
+
+  public int weight() {
+    return weight;
+  }
+
 }
